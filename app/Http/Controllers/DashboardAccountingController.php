b58d53f3ba8df7e2caf3cfd55a006bc69a4f18db
rachmanj <rachmanj@gmail.com>
2022-11-23 11:09:33 +0800

    minor update

--- app/Http/Controllers/DashboardAccountingController.php
+++ app/Http/Controllers/DashboardAccountingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AdvanceCategory;
 use App\Models\Payreq;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,15 +13,21 @@ class DashboardAccountingController extends Controller
 {
     public function index()
     {
+        $dnc_id = User::where('username', 'dncdiv')->first()->id;
         $today = Carbon::now();
         $this_year_outgoings = Payreq::selectRaw('payreq_idr, substring(outgoing_date, 6, 2) as month')
-            ->whereYear('outgoing_date', $today->year)->get();
+            ->whereYear('outgoing_date', $today->year)
+            ->where('user_id', '<>', $dnc_id)
+            ->get();
+
 
         return view('accounting-dashboard.index', [
             'today_outgoings' => Payreq::whereDate('outgoing_date', $today),
-            'this_month_outgoings' => Payreq::whereMonth('outgoing_date', $today),
+            'this_month_outgoings' => Payreq::whereMonth('outgoing_date', $today)->where('user_id', '<>', $dnc_id),
             'months' => $this->get_month(),
             'this_year_outgoings' => $this_year_outgoings,
+            'categories' => AdvanceCategory::orderBy('code', 'asc')->get(),
+            'byCategories' => $this->advance_by_categories(),
         ]);
     }
 
@@ -31,12 +39,26 @@ class DashboardAccountingController extends Controller
             ->get();
     }
 
+    public function advance_by_categories()
+    {
+        $advances = Payreq::selectRaw('advance_category_id, sum(payreq_idr) as total')
+            ->whereYear('approve_date', Carbon::now())
+            ->groupBy('advance_category_id')
+            ->get();
+
+        return $advances;
+    }
+
     public function test()
     {
         $today = Carbon::now();
-        $this_year_outgoings = DB::table('payreqs')->selectRaw('payreq_idr, substring(outgoing_date, 6, 2) as month')
-            ->whereYear('outgoing_date', $today->year)->get();
+        $dnc_id = User::where('username', 'dncdiv')->first()->id;
+        $payreqs = Payreq::whereMonth('outgoing_date', $today)
+            // ->orderBy('payreq_idr', 'desc')
+            ->where('user_id', '<>', $dnc_id)
+            // ->get();
+            ->sum('payreq_idr');
 
-        return $this_year_outgoings;
+        return $payreqs;
     }
 }
---
+++ resources/views/accounting-dashboard/row-3.blade.php
@@ -0,0 +1,31 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card card-info">
+            <div class="card-header border-transparent">
+                <h3 class="card-title"><b>Monthly Advance by Category</b></h3>
+            </div>
+            <div class="card-body p-0">
+                <table class="table m-0 table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Month</th>
+                                @foreach ($months as $month)
+                                    <th>{{ date('M', strtotime('2022-' . $month->month . '-01')) }}</th>
+                                @endforeach
+                        </tr>
+                    </thead>
+                    <tbody>
+                       @foreach ($categories as $category)
+                           <tr>
+                                <td>{{ $category->code }}</td>
+                           </tr>
+                       @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+
+    </div>
+</div>
