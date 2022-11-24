df8f6be84f6115c32ea619acf942af18974d2fe9
rachmanj <rachmanj@gmail.com>
2022-11-24 08:31:31 +0800

    minor update

--- resources/views/accounting-dashboard/row-1.blade.php
+++ resources/views/accounting-dashboard/row-1.blade.php
@@ -2,7 +2,7 @@
     <div class="col-6">
         <div class="card">
             <div class="card-header">
-                <div class="card-title"><h5>Today Outgoing</h5></div>
+                <div class="card-title"><h5>Today Outgoing <small>(non DnC)</small></h5></div>
                 <div class="float-right"><h5>Rp.{{ number_format($today_outgoings->sum('payreq_idr'), 0) }}</h5></div>
             </div>
         </div>
@@ -10,7 +10,7 @@
     <div class="col-6">
         <div class="card">
             <div class="card-header">
-                <div class="card-title"><h5>This Month Outgoing</h5></div>
+                <div class="card-title"><h5>This Month Outgoing <small>(non DnC)</small></h5></div>
                 <div class="float-right"><h5>Rp.{{ number_format($this_month_outgoings->sum('payreq_idr')) }}</h5></div>
             </div>
         </div>
