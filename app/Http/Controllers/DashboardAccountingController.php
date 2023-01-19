833597f666840e9cf8b6cf0ed1fb265fc7b6b872
rachmanj <rachmanj@gmail.com>
2023-01-19 16:31:46 +0800

    minor update

--- app/Http/Controllers/DashboardAccountingController.php
+++ app/Http/Controllers/DashboardAccountingController.php
@@ -17,7 +17,7 @@ class DashboardAccountingController extends Controller
     public function index()
     {
         $dnc_id = User::where('username', 'dncdiv')->first()->id;
-        $today = Carbon::now()->addHours(8);
+        $today = Carbon::now()->subHours(8);
 
         return view('accounting-dashboard.index', [
             'today_outgoings' => Payreq::whereYear('outgoing_date', $today)->whereMonth('outgoing_date', $today)->whereDate('outgoing_date', $today),
